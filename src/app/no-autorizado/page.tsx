97684d8bac19ef8804917070efb662a53497ce36
carlos viveros <carlos06182016@gmail.com>
2025-04-17 14:39:33 -0500

    update router

--- src/app/no-autorizado/page.tsx
+++ src/app/no-autorizado/page.tsx
@@ -1,18 +1,30 @@
-'use client';
-
-import Link from 'next/link';
+"use client";
+import "@/services/amplify-config";
+import { signOut } from "@aws-amplify/auth";
+import { useRouter } from "next/navigation";
 
 export default function NoAutorizadoPage() {
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await signOut();
+    router.push("/dashboard");
+  };
+
   return (
     <main className="d-flex justify-content-center align-items-center vh-100 bg-light">
-      <div className="card text-center shadow p-4" style={{ maxWidth: '500px' }}>
+      <div
+        className="card text-center shadow p-4"
+        style={{ maxWidth: "500px" }}
+      >
         <h1 className="text-danger mb-3">Acceso Denegado</h1>
         <p className="text-muted mb-4">
           No tienes permisos para acceder a esta página.
         </p>
-        <Link href="/dashboard" className="btn btn-danger">
+        <button onClick={handleSubmit} className="btn btn-danger">
           Volver al inicio
-        </Link>
+        </button>
       </div>
     </main>
   );
--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -1,47 +1,49 @@
-
 type Ruta = {
-    nombre: string;
-    ruta: string;
+  nombre: string;
+  ruta: string;
 };
 //"dashboard" |
-export type Modulo =  "clientes" | "categorias" | "proveedores" | "productos" | "usuarios" | "metodos-pagos" | "ventas" | "cambiar-clave" | "finanzas" | "configuracion";
+export type Modulo =
+  | "dashboard"
+  | "clientes"
+  | "categorias"
+  | "proveedores"
+  | "productos"
+  | "usuarios"
+  | "metodos-pagos"
+  | "ventas"
+  | "cambiar-clave"
+  | "finanzas"
+  | "configuracion";
 
 export const rutasPorModulo: Record<Modulo, Ruta[]> = {
-    //dashboard: [
-    //    { nombre: "Dashboard", ruta: "/dashboard" },
-    //],
-    clientes: [
-        { nombre: "Lista de Clientes", ruta: "/dashboard/clientes/lista" },
-        { nombre: "Registrar Clientes", ruta: "/dashboard/clientes/nuevo" },
-    ],
-    categorias: [
-        { nombre: "Categorías", ruta: "/dashboard/categorias" },
-    ],
-    proveedores: [
-        { nombre: "Lista de Proveedores", ruta: "/dashboard/proveedores/lista" },
-        { nombre: "Registrar Proveedores", ruta: "/dashboard/proveedores/nuevo" },
-    ],
-    productos: [
-        { nombre: "Lista de Productos", ruta: "/dashboard/productos/lista" },
-        { nombre: "Registrar Productos", ruta: "/dashboard/productos/nuevo" },
-    ],
-    usuarios: [
-        { nombre: "Lista de Usuarios", ruta: "/dashboard/usuarios/lista" },
-        { nombre: "Registrar Usuarios", ruta: "/dashboard/usuarios/nuevo" },
-    ],
-    "metodos-pagos": [
-        { nombre: "Métodos de Pago", ruta: "/dashboard/metodos-pago" },
-    ],
-    ventas: [
-        { nombre: "Registrar Ventas", ruta: "/dashboard/ventas/nuevo" },
-    ],
-    "cambiar-clave": [
-        { nombre: "Cambiar Contraseña", ruta: "/dashboard/cambiar-clave" },
-    ],
-    finanzas: [
-        { nombre: "Finanzas", ruta: "/dashboard/finanzas" },
-    ],
-    configuracion: [
-        { nombre: "Configuraciones", ruta: "/dashboard/configuracion" },
-    ],
+  dashboard: [{ nombre: "Dashboard", ruta: "/dashboard" }],
+  clientes: [
+    { nombre: "Lista de Clientes", ruta: "/dashboard/clientes/lista" },
+    { nombre: "Registrar Clientes", ruta: "/dashboard/clientes/nuevo" },
+  ],
+  categorias: [{ nombre: "Categorías", ruta: "/dashboard/categorias" }],
+  proveedores: [
+    { nombre: "Lista de Proveedores", ruta: "/dashboard/proveedores/lista" },
+    { nombre: "Registrar Proveedores", ruta: "/dashboard/proveedores/nuevo" },
+  ],
+  productos: [
+    { nombre: "Lista de Productos", ruta: "/dashboard/productos/lista" },
+    { nombre: "Registrar Productos", ruta: "/dashboard/productos/nuevo" },
+  ],
+  usuarios: [
+    { nombre: "Lista de Usuarios", ruta: "/dashboard/usuarios/lista" },
+    { nombre: "Registrar Usuarios", ruta: "/dashboard/usuarios/nuevo" },
+  ],
+  "metodos-pagos": [
+    { nombre: "Métodos de Pago", ruta: "/dashboard/metodos-pago" },
+  ],
+  ventas: [{ nombre: "Registrar Ventas", ruta: "/dashboard/ventas/nuevo" }],
+  "cambiar-clave": [
+    { nombre: "Cambiar Contraseña", ruta: "/dashboard/cambiar-clave" },
+  ],
+  finanzas: [{ nombre: "Finanzas", ruta: "/dashboard/finanzas" }],
+  configuracion: [
+    { nombre: "Configuraciones", ruta: "/dashboard/configuracion" },
+  ],
 };
