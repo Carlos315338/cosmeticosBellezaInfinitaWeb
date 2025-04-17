82a6ad949a12af62efdf338a489ae79c704a6917
carlos viveros <carlos06182016@gmail.com>
2025-04-17 14:11:01 -0500

    update page auth

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,17 +1,16 @@
-'use client';
-import '@/services/amplify-config';
-import { useRouter } from 'next/navigation';
-import Image from 'next/image';
-import { useState } from 'react';
-import { getCurrentUser, signIn, signOut } from '@aws-amplify/auth';
-import { useAuth } from '@/context/AuthContext';
-import { usuarioService } from '@/services/usuarios/usuarioService';
+"use client";
+import "@/services/amplify-config";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { useState } from "react";
+import { getCurrentUser, signIn, signOut } from "@aws-amplify/auth";
+import { useAuth } from "@/context/AuthContext";
+import { usuarioService } from "@/services/usuarios/usuarioService";
 
 export default function LoginPage() {
-
   const router = useRouter();
-  const [idNumber, setIdNumber] = useState('');
-  const [password, setPassword] = useState('');
+  const [idNumber, setIdNumber] = useState("");
+  const [password, setPassword] = useState("");
 
   const { setAuthData } = useAuth();
 
@@ -20,28 +19,33 @@ export default function LoginPage() {
 
     try {
       const user = await getCurrentUser();
-      console.log('Ya hay un usuario logueado:', user);
-      await signOut(); 
+      console.log("Ya hay un usuario logueado:", user);
+      await signOut();
     } catch {
       // No hay sesión activa, está bien continuar
     }
 
     try {
       //const user = await Auth.signIn(idNumber, password);
-      const user = await signIn({ username: idNumber, password });
+      await signIn({ username: idNumber, password });
       const usuarioLogueado = await usuarioService.obtenerPorId(idNumber);
       setAuthData(usuarioLogueado);
-      router.push('/dashboard');
+      router.push("/dashboard");
     } catch (error) {
-      console.error('Error de login', error);
-      alert('Credenciales inválidas');
+      console.error("Error de login", error);
+      alert("Credenciales inválidas");
     }
   };
-  
 
   return (
     <main className="login-container">
-      <Image src="/logo.png" alt="Logo" width={180} height={180} className="login-container__logo" />
+      <Image
+        src="/logo.png"
+        alt="Logo"
+        width={240}
+        height={240}
+        className="login-container__logo"
+      />
 
       <form onSubmit={handleSubmit} className="login-box">
         <label htmlFor="idNumber" className="login-box__label">
